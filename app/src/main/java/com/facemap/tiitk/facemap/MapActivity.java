14bc70309e8b7ec7881e40054c074014c3eed9eb
DigiTund <tiitkroon@gmail.com>
2017-08-08 17:09:56 +0300

    first

---
+++ app/src/main/java/com/facemap/tiitk/facemap/MapActivity.java
@@ -0,0 +1,312 @@
+package com.facemap.tiitk.facemap;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facemap.tiitk.facemap.POJO.Travel;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+import static java.lang.Double.parseDouble;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    public LatLng middlePoint= null;
+    private GoogleMap mMap;
+    LatLng origin;
+    LatLng dest;
+    ArrayList<LatLng> MarkerPoints;
+    Polyline line;
+    //CallbackManager callbackManager;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+
+        // Initializing
+        MarkerPoints = new ArrayList<>();
+
+        //show error dialog if Google Play Services not available
+        if (!isGooglePlayServicesAvailable()) {
+            Log.d("onCreate", "Google Play Services not available. Ending Test case.");
+            finish();
+        }
+        else {
+            Log.d("onCreate", "Google Play Services available. Continuing.");
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng Model_Town = new LatLng(60.1699, 24.9384);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(Model_Town, 7));
+
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(Model_Town));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+        // Setting onclick event listener for the map
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng point) {
+
+                // clearing map and generating new marker points if user clicks on map more than two times
+                if (MarkerPoints.size() > 1) {
+                    mMap.clear();
+                    MarkerPoints.clear();
+                    MarkerPoints = new ArrayList<>();
+
+                }
+
+                // Adding new item to the ArrayList
+                MarkerPoints.add(point);
+
+                // Creating MarkerOptions
+                MarkerOptions options = new MarkerOptions();
+
+                // Setting the position of the marker
+                options.position(point);
+
+                /*
+                 * For the start location, the color of marker is GREEN and
+                 * for the end location, the color of marker is RED.
+                 */
+                if (MarkerPoints.size() == 1) {
+                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                } else if (MarkerPoints.size() == 2) {
+                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+
+                }
+                // Add new marker to the Google Map Android API V2
+                mMap.addMarker(options);
+
+                // Checks, whether start and end locations are captured
+                if (MarkerPoints.size() >= 2) {
+                    origin = MarkerPoints.get(0);
+                    dest = MarkerPoints.get(1);
+                    build_travel_retrofit_and_get_response("driving");
+                }
+            }
+        });
+    }
+
+    private void build_travel_retrofit_and_get_response(String travelType) {
+
+        String url = "https://maps.googleapis.com/maps/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitMaps service = retrofit.create(RetrofitMaps.class);
+
+        Call<Travel> callTravel = service.getDistanceDuration("metric", origin.latitude + "," + origin.longitude,dest.latitude + "," + dest.longitude, travelType);
+        callTravel.enqueue(new Callback<Travel>() {
+            @Override
+            public void onResponse(Response<Travel> response, Retrofit retrofit) {
+
+                try {
+                    //Remove previous line from map
+                    if (line != null) {
+                        line.remove();
+                    }
+                    // This loop will go through all the results and add marker on each location.
+                    for (int i = 0; i < response.body().getRoutes().size(); i++) {
+                        String encodedString = response.body().getRoutes().get(0).getOverviewPolyline().getPoints();
+                        List<LatLng> list = decodePoly(encodedString);
+                        middlePoint = list.get((int) Math.ceil(list.size() / 2));
+                        line = mMap.addPolyline(new PolylineOptions()
+                                .addAll(list)
+                                .width(20)
+                                .color(Color.RED)
+                                .geodesic(true)
+                        );
+                    }
+                    fbApiCall();
+                } catch (Exception e) {
+                    Log.d("onResponse", "There is an error");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.d("onFailure", t.toString());
+            }
+        });
+    }
+    private void fbApiCall(){
+
+        GraphRequest GraphRequest = new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/search",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        JSONObject jsonResponse = response.getJSONObject();
+                        Log.e("error",""+response);
+                            try {
+                            JSONArray jsonArray = jsonResponse.getJSONArray("data");
+                           for(int i=0;i<jsonArray.length();i++){
+                               JSONObject cafe = jsonArray.getJSONObject(i);
+                               String lat = cafe.getJSONObject("location").getString("latitude");
+                               String lng = cafe.getJSONObject("location").getString("longitude");
+                               MarkerOptions markerOptions = new MarkerOptions();
+                               LatLng latLng = new LatLng(parseDouble(lat),parseDouble(lng));
+                               markerOptions.position(latLng);
+                                // Adding Title to the Marker
+                               markerOptions.title(cafe.getString("name")+" : Likes:"+cafe.getString("fan_count"));
+                               // Adding colour to the marker
+                               markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                                // Adding Marker to the Camera.
+                               mMap.addMarker(markerOptions);
+                           }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        Bundle parameters = new Bundle();
+        parameters.putString("type","place");
+        parameters.putString("q","cafe");
+        parameters.putString("center",""+middlePoint.latitude+","+middlePoint.longitude);
+        parameters.putString("fields","fan_count,name, location");
+        parameters.putString("limit","15");
+        GraphRequest.setParameters(parameters);
+        GraphRequest.executeAsync();
+    }
+
+    private List<LatLng> decodePoly(String encoded) {
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng( (((double) lat / 1E5)),
+                    (((double) lng / 1E5) ));
+            poly.add(p);
+        }
+        return poly;
+    }
+
+    // Checking if Google Play Services Available or not
+    private boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int result = googleAPI.isGooglePlayServicesAvailable(this);
+        if(result != ConnectionResult.SUCCESS) {
+            if(googleAPI.isUserResolvableError(result)) {
+                googleAPI.getErrorDialog(this, result,
+                        0).show();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    public boolean checkLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/com/facemap/tiitk/facemap/POJO/Route.java
@@ -0,0 +1,34 @@
+package com.facemap.tiitk.facemap.POJO;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Route {
+
+
+    @SerializedName("overview_polyline")
+    @Expose
+    private OverviewPolyline overviewPolyline;
+
+    /**
+     *
+     * @return
+     * The overviewPolyline
+     */
+    public OverviewPolyline getOverviewPolyline() {
+        return overviewPolyline;
+    }
+
+    /**
+     *
+     * @param overviewPolyline
+     * The overview_polyline
+     */
+    public void setOverviewPolyline(OverviewPolyline overviewPolyline) {
+        this.overviewPolyline = overviewPolyline;
+    }
+
+}
